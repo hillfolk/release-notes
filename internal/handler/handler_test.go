165adfd80b2073808b9ee05fe877fc7a5dc574d3
JunYong <pjy1418@gmail.com>
2023-12-25 02:56:23 +0900

    ISSUE07: handler 테스트 추가

---
+++ internal/handler/handler_test.go
@@ -0,0 +1,20 @@
+package handler
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestHello(t *testing.T) {
+	handler := http.HandlerFunc(Hello)
+	r := httptest.NewRequest("GET", "/hello", nil)
+	w := httptest.NewRecorder()
+	handler(w, r)
+	resp := w.Result()
+	if resp.StatusCode != http.StatusOK {
+		t.Errorf("Expected: %d, Got: %d", http.StatusOK, resp.StatusCode)
+	}
+	t.Logf("Status code: %d", resp.StatusCode)
+	t.Logf("Response: %v", resp.Body)
+}
